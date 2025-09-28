8ca9002ab5f39787c9673494b05b8a8474c40c29
André Balázs <andrebalazs13@gmail.com>
2025-09-28 12:50:51 +0200

    Move ordering has been optimized

--- engine/move_ordering.py
+++ engine/move_ordering.py
@@ -1,12 +1,15 @@
 import chess
 from config import killer_moves, PIECE_VALUES
 
-def order_moves(board, moves):
+def order_moves(board, moves, depth=None):
+    legal_moves_list = list(moves)
 
     killer_moves_ordered = []
-    for move in killer_moves:
-        if move in list(moves):
-            killer_moves_ordered.append(move)
+
+    if depth and killer_moves[depth]:
+        for move in killer_moves[depth]:
+            if move in legal_moves_list:
+                killer_moves_ordered.append(move)
 
     remaining_moves = [m for m in moves if m not in killer_moves_ordered]
 
@@ -16,7 +19,7 @@ def order_moves(board, moves):
             attacker = board.piece_at(move.from_square)
             victim_value = PIECE_VALUES[victim.piece_type] if victim else 0
             attacker_value = PIECE_VALUES[attacker.piece_type] if attacker else 0
-            return 10 * victim_value - attacker_value
+            return 10 * (victim_value - attacker_value) + 100
 
         board.push(move)
         check = 1 if board.is_check() else 0
