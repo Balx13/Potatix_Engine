6088b3000fc4522687d01c49c62de0a9c4bc193f
André Balázs <andrebalazs13@gmail.com>
2025-10-26 12:06:03 +0100

    Added estimate_moves_to_go

--- engine/time_manager.py
+++ engine/time_manager.py
@@ -1,14 +1,33 @@
 import chess
 from config import PIECE_VALUES
+
+def estimate_moves_to_go(board: chess.Board) -> int:
+
+    piece_count = len(board.piece_map())
+    moves_based_on_material = max(5, piece_count // 2)
+
+    fullmove = board.fullmove_number
+    if fullmove < 15:
+        phase_factor = 1.5
+    elif fullmove < 30:
+        phase_factor = 1.0
+    else:
+        phase_factor = 0.7
+
+    estimated_moves_to_go = max(5, int(moves_based_on_material * phase_factor))
+
+    return estimated_moves_to_go
+
+
 def estimate_time_for_move(board: chess.Board, base_time: float, increment: float, moves_to_go=None) -> float:
 
     move_number = board.fullmove_number
     if move_number < 15:
-        phase_factor = 0.6
+        phase_factor = 0.9
     elif move_number < 40:
         phase_factor = 1.0
     else:
-        phase_factor = 1.3
+        phase_factor = 1.2
 
     material_balance = 0
 
@@ -17,19 +36,16 @@ def estimate_time_for_move(board: chess.Board, base_time: float, increment: floa
         material_balance -= len(board.pieces(piece_type, chess.BLACK)) * value
 
     if abs(material_balance) <= 200:
-        complexity_factor = 1.2  # kiegyenlítettebb állás → több gondolkodás
+        complexity_factor = 1.2
     else:
-        complexity_factor = 0.8  # nagyobb előny egyik félnél → kevesebb idő is elég
+        complexity_factor = 0.9
 
-    # 3. Időelosztás (átlagos lépésszámra osztva)
     if not moves_to_go:
-        moves_to_go = 40
+        moves_to_go = estimate_moves_to_go(board)
     base_allocation = (base_time / moves_to_go) + increment
 
-    # 4. Végső idő számítás
     allocated_time = base_allocation * phase_factor * complexity_factor
 
-    # 5. Minimum és maximum korlát
     allocated_time = max(0.05, min(allocated_time, base_time * 0.5))
 
     return allocated_time
