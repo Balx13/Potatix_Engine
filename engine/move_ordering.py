967e99eff8713b85a6674f08b470064d63816655
André Balázs <andrebalazs13@gmail.com>
2025-11-01 13:33:45 +0100

    Optimized move_ordering.py

--- engine/move_ordering.py
+++ engine/move_ordering.py
@@ -3,7 +3,74 @@ from config import killer_moves, PIECE_VALUES, history_heuristic, counter_styles
 from evulate import evaluate_board
 from adaptive_style import playing_style_recognition
 
-def order_moves(board, moves, depth=None, datas_for_evulate=None):
+
+def sorted_moves_with_value(moves, board, datas_for_evulate):
+    sortedMoves = []
+    for move in moves:
+        moves_score = score(move, board, datas_for_evulate)
+
+        inserted = False
+        for i in range(len(sortedMoves)):
+            if moves_score > sortedMoves[i][1]:
+                sortedMoves.insert(i, (move, moves_score))
+                inserted = True
+                break
+
+        if not inserted:
+            sortedMoves.append((move, moves_score))
+
+    return sortedMoves
+
+
+def followed_style(board, datas_for_evulate):
+    engine_is_white = datas_for_evulate[1]
+    _, musters = evaluate_board(board, with_muster=True)
+
+    opponent_style = datas_for_evulate[0]
+    motor_to_move = (engine_is_white == board.turn)
+    expected_style = counter_styles[opponent_style] if motor_to_move else opponent_style
+
+    if playing_style_recognition(musters, board.turn) == expected_style:
+        return True
+    else:
+        return False
+
+def score(move_, board, datas_for_evulate):
+
+    piece = board.piece_at(move_.from_square)
+    followed_s = followed_style(board, datas_for_evulate)
+    if piece:
+        piece_type = piece.piece_type
+        piece_type -= 1
+        history_score = history_heuristic[piece_type][move_.from_square][move_.to_square]
+    else:
+        history_score = 0
+
+    if board.is_capture(move_): # Ütés
+        victim = board.piece_at(move_.to_square)
+        attacker = board.piece_at(move_.from_square)
+        victim_value = PIECE_VALUES[victim.piece_type] if victim else 0
+        attacker_value = PIECE_VALUES[attacker.piece_type] if attacker else 0
+        if followed_s:
+            return 10 * (victim_value - attacker_value) + 350 + history_score # 350-
+        else:
+            return 10 * (victim_value - attacker_value) + 300 + history_score  # 300-
+
+    board.push(move_)
+    if board.is_check(): # Sakk
+        board.pop()
+        if followed_s:
+            return 150 + history_score # 150-250
+        else:
+            return 100  + history_score# 100-200
+    board.pop()
+
+    if followed_s: # Maradék
+        return 5 + history_score # 5-105
+    else:
+        return 0 + history_score # 0-100
+
+def order_moves(board, moves, depth, datas_for_evulate):
     if datas_for_evulate is None:
         datas_for_evulate = []
     legal_moves_list = list(moves)
@@ -13,56 +80,8 @@ def order_moves(board, moves, depth=None, datas_for_evulate=None):
     if depth and killer_moves[depth]:
         for move in killer_moves[depth]:
             if move in legal_moves_list:
-                killer_moves_ordered.append(move)
+                killer_moves_ordered.append((move, 1000))
 
     remaining_moves = [m for m in moves if m not in killer_moves_ordered]
 
-    def followed_style():
-        engine_is_white = datas_for_evulate[1]
-        _, musters = evaluate_board(board, with_muster=True)
-
-        opponent_style = datas_for_evulate[0]
-        motor_to_move = (engine_is_white == board.turn)
-        expected_style = counter_styles[opponent_style] if motor_to_move else opponent_style
-
-        if playing_style_recognition(musters, board.turn) == expected_style:
-            return True
-        else:
-            return False
-
-    def score(move_):
-        piece = board.piece_at(move_.from_square)
-        followed_s = followed_style()
-        if piece:
-            piece_type = piece.piece_type
-            piece_type -= 1
-            history_score = history_heuristic[piece_type][move_.from_square][move_.to_square]
-        else:
-            history_score = 0
-
-        if board.is_capture(move_): # Ütés
-            victim = board.piece_at(move_.to_square)
-            attacker = board.piece_at(move_.from_square)
-            victim_value = PIECE_VALUES[victim.piece_type] if victim else 0
-            attacker_value = PIECE_VALUES[attacker.piece_type] if attacker else 0
-            if followed_s:
-                return 10 * (victim_value - attacker_value) + 350 + history_score # 350-
-            else:
-                return 10 * (victim_value - attacker_value) + 300 + history_score  # 300-
-
-        board.push(move_)
-        if board.is_check(): # Sakk
-            board.pop()
-            if followed_s:
-                return 150 + history_score # 150-250
-            else:
-                return 100  + history_score# 100-200
-        board.pop()
-
-        if followed_s: # Maradék
-            return 5 + history_score # 5-105
-        else:
-            return 0 + history_score # 0-100
-
-    remaining_moves = sorted(remaining_moves, key=score, reverse=True)
-    return killer_moves_ordered + remaining_moves
+    return killer_moves_ordered + sorted_moves_with_value(remaining_moves, board, datas_for_evulate)
