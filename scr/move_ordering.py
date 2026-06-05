21da1a7c2aca52ab8f54b177d6f57d165bb2e622
Balx13 <andrebalazs13@gmail.com>
2026-06-05 17:31:59 +0200

    Removed adaptive mode from move ordering

--- scr/move_ordering.py
+++ scr/move_ordering.py
@@ -41,65 +41,39 @@ def sorted_moves_with_value(moves, board: chess.Board) ->list:
 
 
 def history_score(board: chess.Board, move_):
-    # Megmondja egy adott lépésnek a history table score-ját
     piece = board.piece_at(move_.from_square)
-
     if piece:
-        piece_type = piece.piece_type
-        piece_type -= 1
-        history_score_ = config.history_heuristic[piece_type][move_.from_square][move_.to_square]
+        return config.history_heuristic[piece.piece_type-1][move_.from_square][move_.to_square]
     else:
-        history_score_ = 0
-    return history_score_
+        return 0
 
 def score(move_, board: chess.Board):
     # pontozza az adott lépést
-    m = config.multipliers["engine"] if config.engine_turn else config.multipliers["oppoment"]
+    """
+    capture: -100-10000
+    check: 500-600
+    quiet move: 100-200
+    """
     piece = board.piece_at(move_.from_square)
-    pt = piece.piece_type if piece else None
     history_score_ = history_score(board, move_)
     if board.is_capture(move_): # Ütés
         victim = board.piece_at(move_.to_square)
-        attacker = piece # piece = board.piece_at(move_.from_square)
+        attacker = piece
         victim_value = config.PIECE_VALUES[victim.piece_type] if victim else 0
         attacker_value = config.PIECE_VALUES[attacker.piece_type] if attacker else 0
-        return 1000 + 10 * victim_value - attacker_value + history_score_
+        return 1000 + 10 * max(victim_value - attacker_value, -100) + history_score_
 
     board.push(move_)
     is_check = board.is_check()
     board.pop()
     if is_check:
-        check_bonus = 100 * m.get("king_safety", 1.0)
-        return 500 + check_bonus + history_score_
+        return 500 + history_score_
 
-    quiet_score = 10 + history_score_
-    if pt == chess.BISHOP:
-        quiet_score += m.get("bishop_pairs", 1.0) * 100
-    elif pt == chess.ROOK:
-        quiet_score += m.get("rook_op_files", 1.0) * 100
-
-    quiet_score += m.get("mobility", 1.0) * 50
-    quiet_score += 100
+    quiet_score = 100 + history_score_
 
     return quiet_score
 
 def order_moves(board: chess.Board, moves, depth: int) -> list:
-    """
-
-    :param board: chess.Board
-    :param moves: chess.Move
-    :param depth: int
-    :return: list of moves
-
-    Move ordering
-
-    Move weights:
-    - Killer moves: 10000-10100
-    - Capture moves: 1000-9100
-    - Check moves: 570-730
-    - Quiet moves: 145-405
-
-    """
     # Visszaadja a sorbarendezett lépéseket
 
     legal_moves_list = list(moves)
